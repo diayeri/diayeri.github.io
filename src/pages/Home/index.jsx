1605fa2e1082bba9ab8bfd1405016019517c35dd
diayeri <jdyoung1031@gmail.com>
2024-03-24 22:23:14 +0900

    feat: Create basic pages

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>My Page</h1>
+      <p>This is my page content.</p>
+    </div>
+  );
+};
+
+export default Home;
